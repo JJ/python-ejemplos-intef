3873d7c11539d4601f8760c575a7c2779ad18f30
JJ Merelo <jjmerelo@gmail.com>
2021-07-09 14:30:22 +0200

    Otro

---
+++ randoms-while.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import random
+
+uno_al_tres = random.randint(1,3)
+while uno_al_tres != 3:
+    uno_al_tres = random.randint(1,3)
+    print(f"Otra vez será, salió {uno_al_tres}")
+
+print("A la tercera fue la vencida")
