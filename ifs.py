df9eb441b5c1b917031ec03a76e281cccd19a7a1
JJ Merelo <jjmerelo@gmail.com>
2021-07-12 14:34:55 +0200

    Final version

--- ifs.py
+++ ifs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-numero = 6
-if numero // 2:
+numero = 9
+if numero // 2 == numero / 2 :
    print("Par")
-   if ( numero // 3):
-      print( "Divisible por tres")
+if numero // 3 == numero /3:
+   print( "Divisible por tres")
