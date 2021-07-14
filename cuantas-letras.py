fe1d2396397dd9ff2a39a66f54e8dec6ef0f7399
JJ Merelo <jjmerelo@gmail.com>
2021-07-14 18:28:00 +0200

    Con comentarios

--- cuantas-letras.py
+++ cuantas-letras.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-def cuantas_letras(frase: str) -> int:
+def cuantas_letras(frase):
+    '''
+    Args:
+        frase: Una frase en una cadena, que no deberá contener signos de puntuación.
+    Returns:
+        el número de caracteres en las palabras de la frase.
+    '''
     palabras = frase.split(" ")
     letras = 0
     for p in palabras:
@@ -9,6 +15,5 @@ def cuantas_letras(frase: str) -> int:
     return f"La frase «{frase}» tiene {letras} letras"
 
 
-print(cuantas_letras(33))
 print(cuantas_letras("En un lugar de La Mancha"))
 print(cuantas_letras("Algo huele mal en Dinamarca"))
